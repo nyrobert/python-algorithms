074f7c8f82ddb643c37ae6603762d2b047db6f99
nyrobert <nyitrai.robert@gmail.com>
2026-04-16 18:51:20 +0200

    add linear search

---
+++ linear_search.py
@@ -0,0 +1,19 @@
+"""
+Linear Sort
+
+- searches through an array and returns the index of the value it searches for
+- Big-O:
+    - worst case: O(n)
+    - average case: O(n)
+    - best case: O(1)
+"""
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+numbers = [5, 9, 7, 3, 10, 8, 1, 6, 4, 2]
+
+print(linear_search(numbers, 7))
+print(linear_search(numbers, 11))
